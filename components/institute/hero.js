216773885d47ef52e651af43bebb46678d58542a
Jecy Jehu <jecyjehu@outlook.com>
2023-06-21 20:38:25 +0100

    deploy

--- components/institute/hero.js
+++ components/institute/hero.js
@@ -3,27 +3,7 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <section className=" w-full h-auto mt-10">
-      <div className="grid content-center pt-72 sm:pt-56">
-        <div className='z-[-2]'>
-          <Image src='/assets/images/cone.png' alt='/' fill cover />
-        </div>
-        <div className='max-w-[520px] sm:max-w-[620px] md:max-w-[1250px] w-full m-auto space-y-14 sm:space-y-8 text-white'>
-        <h1 className='text-base lg:text-3xl'>Personalized, Proactive, Preventive care</h1>
-        <div className='text-5xl lg:text-9xl'>
-          <h1>The future of</h1>
-          <h1>medicine today.</h1>
-        </div>
-      </div>
-      </div>
-    </section>
-  )
-}
-
-export default Hero;
-
-
-{/* <section className='grid content-center pt-72 w-full h-screen'>
+    <section className='grid content-center pt-72 sm:pt-44 w-full h-screen'>
       <div className='z-[-2]'>
         <Image src='/assets/images/cone.png' alt='/' fill cover />
       </div>
@@ -34,4 +14,11 @@ export default Hero;
             <h1>medicine today.</h1>
         </div>
       </div>
-    </section> */}
+    </section>
+  )
+}
+
+export default Hero;
+
+
+{/*  */}
