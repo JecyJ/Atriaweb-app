f4f6dfbbed139e19e629e3997d8cf795da8403f7
Jecy Jehu <jecyjehu@outlook.com>
2023-06-21 20:10:28 +0100

    deploy

--- components/institute/hero.js
+++ components/institute/hero.js
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <section className='grid content-center pt-72 w-full h-screen'>
       <div className='z-[-2]'>
-        <Image src='/assets/images/cone.png' alt='/' layout='fill' objectFit='cover' />
+        <Image src='/assets/images/cone.png' alt='/' fill cover />
       </div>
       <div className='max-w-[520px] sm:max-w-[620px] md:max-w-[1250px] w-full m-auto space-y-14 text-white'>
         <h1 className='text-lg md:text-3xl'>Personalized, Proactive, Preventive care</h1>
