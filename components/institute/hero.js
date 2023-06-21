d8ae6f7950a3b245b82694601180decf17d13bbb
Jecy Jehu <jecyjehu@outlook.com>
2023-06-21 20:25:35 +0100

    deploy

--- components/institute/hero.js
+++ components/institute/hero.js
@@ -3,19 +3,35 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <section className='grid content-center pt-72 w-full h-screen'>
-      <div className='z-[-2]'>
-        <Image src='/assets/images/cone.png' alt='/' fill cover />
-      </div>
-      <div className='max-w-[520px] sm:max-w-[620px] md:max-w-[1250px] w-full m-auto space-y-14 text-white'>
-        <h1 className='text-lg md:text-3xl'>Personalized, Proactive, Preventive care</h1>
-        <div className='text-6xl lg:text-9xl'>
-            <h1>The future of</h1>
-            <h1>medicine today.</h1>
+    <section className=" w-full h-auto mt-10">
+      <div className="grid content-center pt-72 sm:pt-56">
+        <div className='z-[-2]'>
+          <Image src='/assets/images/cone.png' alt='/' fill cover />
         </div>
+        <div className='max-w-[520px] sm:max-w-[620px] md:max-w-[1250px] w-full m-auto space-y-14 sm:space-y-8 text-white'>
+        <h1 className='text-base lg:text-3xl'>Personalized, Proactive, Preventive care</h1>
+        <div className='text-5xl lg:text-9xl'>
+          <h1>The future of</h1>
+          <h1>medicine today.</h1>
+        </div>
+      </div>
       </div>
     </section>
   )
 }
 
 export default Hero;
+
+
+{/* <section className='grid content-center pt-72 w-full h-screen'>
+      <div className='z-[-2]'>
+        <Image src='/assets/images/cone.png' alt='/' fill cover />
+      </div>
+      <div className='max-w-[520px] sm:max-w-[620px] md:max-w-[1250px] w-full m-auto space-y-14 sm:space-y-8 text-white'>
+        <h1 className='text-lg lg:text-3xl'>Personalized, Proactive, Preventive care</h1>
+        <div className='text-6xl lg:text-9xl'>
+            <h1>The future of</h1>
+            <h1>medicine today.</h1>
+        </div>
+      </div>
+    </section> */}
