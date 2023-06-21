a2bb35947bd4c3469e30f51367984500d2fb82a7
Jecy Jehu <jecyjehu@outlook.com>
2023-06-21 17:38:39 +0100

    deploy

---
+++ app/atriainstitute/page.js
@@ -0,0 +1,39 @@
+import Livingroom from '@components/institute/livingroom';
+import Hero from '../../components/institute/hero';
+import Scientists from '../../components/institute/scientists';
+import Science from '@components/institute/science';
+import Medicine from '@components/institute/medicine';
+import Subscribe from '@components/atria/subscribe';
+import Contact from '@components/institute/contact';
+import Manhattan from '@components/institute/manhattan';
+import Doctors from '@components/institute/doctors';
+import Hands from '@components/institute/hands';
+import Nurses from '@components/institute/nurses';
+import Doll from '@components/institute/doll';
+import Mri from '@components/institute/mri';
+import Luggage from '@components/institute/luggage';
+import Art from '@components/institute/art';
+
+const Atriainstitute = () => {
+  return (
+    <section>
+      {/* <Hero /> */}
+      {/* <Scientists /> */}
+      {/* <Livingroom /> */}
+      {/* <Science /> */}
+      {/* <Medicine /> */}
+      {/* <Doctors /> */}
+      {/* <Hands /> */}
+      {/* <Nurses /> */}
+      {/* <Mri /> */}
+      {/* <Doll /> */}
+      {/* <Luggage /> */}
+      {/* <Art /> */}
+      {/* <Manhattan /> */}
+      {/* <Contact /> */}
+      {/* <Subscribe /> */}
+    </section>
+  )
+}
+
+export default Atriainstitute;
