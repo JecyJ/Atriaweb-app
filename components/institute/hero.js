ae16f9d4e1a3a2cae8baa82196293598a758fd20
Jecy Jehu <jecyjehu@outlook.com>
2023-06-21 20:02:30 +0100

    deploy

--- components/institute/hero.js
+++ components/institute/hero.js
@@ -9,7 +9,7 @@ const Hero = () => {
       </div>
       <div className='max-w-[520px] sm:max-w-[620px] md:max-w-[1250px] w-full m-auto space-y-14 text-white'>
         <h1 className='text-lg md:text-3xl'>Personalized, Proactive, Preventive care</h1>
-        <div className='text-6xl md:text-9xl'>
+        <div className='text-6xl lg:text-9xl'>
             <h1>The future of</h1>
             <h1>medicine today.</h1>
         </div>
