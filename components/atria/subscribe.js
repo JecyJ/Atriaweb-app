934b55bd2d1db36b0fbfe5c9f4ad9d17e17d2e1a
Jecy Jehu <jecyjehu@outlook.com>
2023-06-21 17:25:04 +0100

    deploy

--- components/atria/subscribe.js
+++ components/atria/subscribe.js
@@ -9,12 +9,12 @@ const Subscribe = () => {
         </label>
         <div className='flex '>
           <input
-            className='text-2xl md:text-7xl py-4 pl-3 w-full rounded-full border-none bg-[#e7e4df]'
+            className='text-2xl md:text-5xl py-4 pl-3 w-full rounded-full border-none bg-[#e7e4df]'
             type='Email'
             placeholder='Enter your email here'
             name='email'
           />
-          <button className='ml-3 p-2 md:p-3 w-2/4 border rounded-full border-[#3c4e3d] text-[#3c4e3d] text-lg md:text-4xl'>Subscribe</button>
+          <button className='ml-3 p-2 lg:p-3 w-2/4 border rounded-full border-[#3c4e3d] text-[#3c4e3d] text-lg md:text-2xl'>Subscribe</button>
         </div>            
       </form>  
     </section>
