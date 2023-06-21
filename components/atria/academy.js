66cc1a238958d7230121c0898ecd4988a50fe83e
Jecy Jehu <jecyjehu@outlook.com>
2023-06-21 17:21:14 +0100

    deploy

--- components/atria/academy.js
+++ components/atria/academy.js
@@ -16,7 +16,7 @@ const Academy = () => {
                 <div className="mt-20 space-y-28">
                     <div className='text-[#3c4e3d] text-center space-y-10 max-w-[520px] sm:max-w-[620px] m-auto'>
                         <h3 className='text-base md:text-2xl italic'>Translating science into medicine.</h3>
-                        <h1 className='text-4xl md:text-7xl font-medium'>The Academy of Science & Medicine</h1>
+                        <h1 className='text-4xl md:text-6xl font-medium'>The Academy of Science & Medicine</h1>
                     </div>
                     <div className='text-[#3c4e3d] text-base md:text-xl max-w-[520px] sm:max-w-[620px] m-auto'>
                         <p>The Atria Academy of science & Medicine is made up of more than 50 leading scientist, researchers, and doctors from institutions around the world. The Academy exists to translate science into medicine, accelerating the speed by which breakthrough can reach patients, doctors and the public. The Academy helps clinicians refine diagnostic and treatment protocols as well as disease-prevention strategies, and works with the Atria Collaborative to deliver critical, life-changing information and care.</p>
--- components/atria/institute.js
+++ components/atria/institute.js
@@ -16,7 +16,7 @@ const Institute = () => {
           <div className="mt-20 space-y-28">
             <div className='text-[#3c4e3d] text-center space-y-10 max-w-[520px] sm:max-w-[620px] m-auto'>
               <h3 className='text-base md:text-2xl italic'>Delivering personalized, proactive, preventive care.</h3>
-              <h1 className='text-4xl md:text-7xl font-medium'>The Atria Institute</h1>
+              <h1 className='text-4xl md:text-6xl font-medium'>The Atria Institute</h1>
             </div>
             <div className='text-[#3c4e3d] text-base md:text-xl max-w-[520px] sm:max-w-[620px] m-auto'>
               <p>The Atria Institute provides coordinated health care and comprehensive predictive testing to enable a highly personalized approach to well-being. Our multidisciplinary team of in-house physicians combines compassionate care with leading-edge technology, offering advanced screening for cancer, heart, metabolic, and brain disease at our on-site, state-of-the-art testing and imaging center. We empower our members to take a proactive approach to health and well-being and we provide the suport they need to get there.</p>
