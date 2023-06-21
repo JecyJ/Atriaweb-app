6d118a91428df6a270a785768c7f635da2f9d481
Jecy Jehu <jecyjehu@outlook.com>
2023-06-21 17:16:40 +0100

    deploy

--- app/page.js
+++ app/page.js
@@ -1,7 +1,12 @@
 import Hero from "@components/atria/Hero";
+import Academy from "@components/atria/academy";
 import Bgvideo from "@components/atria/bgvideo";
+import Health from "@components/atria/health";
+import Institute from "@components/atria/institute";
 import Learning from "@components/atria/learning";
+import Medicalminds from "@components/atria/medicalminds";
 import Mission from "@components/atria/mission";
+import Subscribe from "@components/atria/subscribe";
 
 const Atria = () => {
   return (
@@ -10,11 +15,11 @@ const Atria = () => {
       <Bgvideo />
       <Learning />
       <Mission />
-      {/* <Medicalminds /> */}
-      {/* <Institute /> */}
-      {/* <Academy /> */}
-      {/* <Health /> */}
-      {/* <Subscribe /> */}
+      <Medicalminds />
+      <Institute />
+      <Academy />
+      <Health />
+      <Subscribe />
     </section>
   )
 }
