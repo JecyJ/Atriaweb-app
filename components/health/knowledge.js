8d9df06daa5829c5913c72d3f1ea243147d3d924
Jecy Jehu <jecyjehu@outlook.com>
2023-06-22 20:29:21 +0100

    deploy

--- components/health/knowledge.js
+++ components/health/knowledge.js
@@ -4,7 +4,7 @@ const Knowledge = () => {
   return (
     <section className='w-full mb-10 mt-20'>
       <div className='max-w-[520px] md:max-w-[1250px] m-auto h-96 md:h-96 text-[#3c4e3d] border-b border-b-3 border-b-[#3c4e3d] text-center space-y-4'>
-        <h1 className='text-6xl sm:text-8xl lg:text-9xl font-medium'>Delivering life-changing knowledge and care.</h1>
+        <h1 className='text-6xl sm:text-7xl lg:text-9xl font-medium'>Delivering life-changing knowledge and care.</h1>
       </div>
     </section>
   )
