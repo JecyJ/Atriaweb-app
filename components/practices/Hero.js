37e7f1edff5f6a2f211cb91674eee37a68ddfb0a
Jecy Jehu <jecyjehu@outlook.com>
2023-06-22 21:14:15 +0100

    deploy

--- components/practices/Hero.js
+++ components/practices/Hero.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <section className='w-full h-auto pt-48 md:pt-64'>
+    <section className='w-full h-auto pt-48 lg:pt-64'>
         <div className='max-w-[520px] sm:max-w-[620px] md:max-w-[1200px] m-auto'>
             <h1 className='text-5xl sm:text-6xl lg:text-8xl text-[#3c4e3d] md:text-center font-medium'>Notice of <br className='md:hidden' /> Privacy <br /> Practices</h1>
         </div>
