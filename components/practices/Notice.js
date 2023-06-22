e6c88e3ce559caa89771302893fa006bcaf49e18
Jecy Jehu <jecyjehu@outlook.com>
2023-06-22 21:51:10 +0100

    deploy

--- components/practices/Notice.js
+++ components/practices/Notice.js
@@ -7,7 +7,7 @@ const Notice = () => {
         <div className="max-w-[450px] md:max-w-[1200px] m-auto">
             <ul className='border-t border-t-[#3c4e3d] border-t-3  my-20'>
                 <li className='text-[#3c4e3d] py-8'><BsCircleFill size={28}  /></li>
-                <li className='relative md:z-[2] text-[#3c4e3d] text-4xl lg:text-6xl font-semibold'>The Atria Health Collaborative engages the expertise, passion, and resources of the entire Atria team to improve healthcare at scale.</li>
+                <li className='relative md:z-[2] text-[#3c4e3d] text-4xl lg:text-6xl font-semibold'>This notice describes how medical information about you may be used and disclosed and how you can get access to this information. </li>
             </ul>
         </div>
     </section>
