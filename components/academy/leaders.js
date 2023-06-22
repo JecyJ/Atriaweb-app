79b6e968ce1deb1312b3fc35a4d1597503547299
Jecy Jehu <jecyjehu@outlook.com>
2023-06-22 20:01:33 +0100

    deploy

--- components/academy/leaders.js
+++ components/academy/leaders.js
@@ -7,8 +7,8 @@ const Leaders = () => {
     <section className="w-full h-auto">
         <div className='pb-16 h-auto'>
             <div className='relative top-12 max-w-[600px] md:max-w-[1450px] m-auto'>
-                <Image className=' absolute top-48 left-48 md:top-80 md:left-96 w-48 sm:w-56 lg:w-80 h-28 sm:h-32 lg:h-48' src='/assets/images/inscription.png' alt='/' width={200} height={200} />
-                <Image className=' absolute z-[-2] top-0 right-0 md:left-28 w-32 md:w-48 h-32 md:h-48' src='/assets/images/block.png' alt='/' width={200} height={200} />
+                <Image className=' absolute top-48 left-48 md:top-52 md:left-52 lg:top-80 lg:left-96 w-36 lg:w-80 h-20 lg:h-48' src='/assets/images/inscription.png' alt='/' width={200} height={200} />
+                <Image className=' absolute z-[-2] top-10 right-0 md:left-28 w-32 lg:w-48 h-32 lg:h-48' src='/assets/images/block.png' alt='/' width={200} height={200} />
                 <Image className=' absolute top-40 right-0 z-[-2] hidden md:block md:right-28 w-2/6 md:w-2/12 h-48 md:h-2/4' src='/assets/images/man.png' alt='/' width={200} height={200} />
                 <div className="relative text-[#3c4e3d] text-6xl sm:text-7xl lg:text-9xl pt-20 w-full">
                     <h1 className="max-w-[450px] md:hidden ml-auto">Powered by</h1>
