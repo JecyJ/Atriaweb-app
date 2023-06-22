9149fdf5c8d605e0670c7fffd1afe27bbb6e4f22
Jecy Jehu <jecyjehu@outlook.com>
2023-06-22 22:05:47 +0100

    deploy

---
+++ components/practices/Practice.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Practice = () => {
+  return (
+    <section className='w-full h-auto my-20'>
+        <div className='max-w-[520px] sm:max-w-[620px] md:max-w-[1200px] m-auto'>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    </section>
+  )
+}
+
+export default Practice;
