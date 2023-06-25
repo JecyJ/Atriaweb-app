6ef8a8bf1b7d606416d2cc34031767924c0264a1
Jecy Jehu <jecyjehu@outlook.com>
2023-06-25 19:59:36 +0100

    deploy

--- components/practices/Practice.js
+++ components/practices/Practice.js
@@ -22,8 +22,10 @@ const Practice = () => {
                     <h3 className='w-full md:max-w-[300px]'>Permissible Uses and Disclosures Without <br /> Your Written Authorisation</h3>
                     <div className='space-y-7'>
                       <p className='md:max-w-[1100px]'>In certain situations, which we describe in Section IV below, we must obtain your written authorization in order to use and/or disclose your PHI. However, we do not need any type of authorization from you for the following uses and disclosures:</p>
-                      <ul className='list-[upper-roman] text-[#3c4e3d]/60 space-y-7'>
-                        <li>Uses and Disclosures For Treatment, Payment and Health Care Operations. We may use and disclose PHI, but not your “Highly Confidential Information” (defined in Section IV.B below), in order to treat you, obtain payment for services provided to you and conduct our “Health Care Operations” as detailed below:</li>
+                      <ul className='text-[#3c4e3d]/60 space-y-7'>
+                        <li>
+                        A. Uses and Disclosures For Treatment, Payment and Health Care Operations. We may use and disclose PHI, but not your “Highly Confidential Information” (defined in Section IV.B below), in order to treat you, obtain payment for services provided to you and conduct our “Health Care Operations” as detailed below:
+                        </li>
                         <ol className='space-y-7'>
                           <li className='list-disc list-inside'>
                           Treatment. We may use and disclose your PHI to provide treatment, for example, to diagnose and treat your injury or illness. We may also disclose PHI to other health care providers involved in your treatment.
@@ -39,36 +41,82 @@ const Practice = () => {
                           </li>
                         </ol>
                         <li className='space-y-7'>
-                          <p>Disclosure to Relatives, Close Friends and Other Caregivers. We may use or disclose your PHI to a family member, other relative, a close personal friend or any other person identified by you when you are present for, or otherwise available prior to, the disclosure, if we (1) obtain your agreement; (2) provide you with the opportunity to object to the disclosure and you do not object; or (3) reasonably infer that you do not object to the disclosure.</p>
+                          <p>
+                          B. Disclosure to Relatives, Close Friends and Other Caregivers. We may use or disclose your PHI to a family member, other relative, a close personal friend or any other person identified by you when you are present for, or otherwise available prior to, the disclosure, if we (1) obtain your agreement; (2) provide you with the opportunity to object to the disclosure and you do not object; or (3) reasonably infer that you do not object to the disclosure.
+                          </p>
 
                           <p>If you are not present, or the opportunity to agree or object to a use or disclosure cannot practicably be provided because of your incapacity or an emergency circumstance, we may exercise our professional judgment to determine whether a disclosure is in your best interests. If we disclose information to a family member, other relative or a close personal friend, we would disclose only information that we believe is directly relevant to the person's involvement with your health care or payment related to your health care. We may also disclose your PHI in order to notify (or assist in notifying) such persons of your location, general condition, or death.</p>
                         </li>
                         <li>
-                        Public Health Activities. We may disclose your PHI for the following public health activities: (1) to report health information to public health authorities for the purpose of preventing or controlling disease, injury or disability; (2) to report child abuse and neglect to public health authorities or other government authorities authorized by law to receive such reports; (3) to report information about products and services under the jurisdiction of the U.S. Food and Drug Administration; (4) to alert a person who may have been exposed to a communicable disease or may otherwise be at risk of contracting or spreading a disease or condition; and (5) to report information to your employer as required under laws addressing work-related illnesses and injuries or workplace medical surveillance.
+                        C. Public Health Activities. We may disclose your PHI for the following public health activities: (1) to report health information to public health authorities for the purpose of preventing or controlling disease, injury or disability; (2) to report child abuse and neglect to public health authorities or other government authorities authorized by law to receive such reports; (3) to report information about products and services under the jurisdiction of the U.S. Food and Drug Administration; (4) to alert a person who may have been exposed to a communicable disease or may otherwise be at risk of contracting or spreading a disease or condition; and (5) to report information to your employer as required under laws addressing work-related illnesses and injuries or workplace medical surveillance.
                         </li>
                         <li>
-                        Victims of Abuse, Neglect or Domestic Violence. If we reasonably believe you are a victim of abuse, neglect or domestic violence, we may disclose your PHI to a governmental authority, including a social service or protective services agency, authorized by law to receive reports of such abuse, neglect or domestic violence.
+                        D. Victims of Abuse, Neglect or Domestic Violence. If we reasonably believe you are a victim of abuse, neglect or domestic violence, we may disclose your PHI to a governmental authority, including a social service or protective services agency, authorized by law to receive reports of such abuse, neglect or domestic violence.
                         </li>
                         <li>
-                        Health Oversight Activities. We may disclose your PHI to a health oversight agency that oversees the health care system and is charged with responsibility for ensuring compliance with the rules of government health programs, such as Medicare or Medicaid.
+                        E. Health Oversight Activities. We may disclose your PHI to a health oversight agency that oversees the health care system and is charged with responsibility for ensuring compliance with the rules of government health programs, such as Medicare or Medicaid.
                         </li>
                         <li>
-                        Judicial and Administrative Proceedings. We may disclose your PHI in the course of a judicial or administrative proceeding in response to a legal order or other lawful process.
+                        F. Judicial and Administrative Proceedings. We may disclose your PHI in the course of a judicial or administrative proceeding in response to a legal order or other lawful process.
                         </li>
                         <li>
-                        Law Enforcement Officers. We may disclose your PHI to the police or other law enforcement officials as required or permitted by law or in compliance with a court order or a grand jury or administrative subpoena.
+                        G. Law Enforcement Officers. We may disclose your PHI to the police or other law enforcement officials as required or permitted by law or in compliance with a court order or a grand jury or administrative subpoena.
                         </li>
                         <li>
-                        Organ and Tissue Procurement. We may disclose your PHI to organizations that facilitate organ, eye or tissue procedure, banking or transplantation.
+                        H. Organ and Tissue Procurement. We may disclose your PHI to organizations that facilitate organ, eye or tissue procedure, banking or transplantation.
                         </li>
                         <li>
-                        Health or Safety. We may use or disclose your PHI to prevent or lessen a serious and imminent threat to a person's or the public's health or safety.
+                        I. Health or Safety. We may use or disclose your PHI to prevent or lessen a serious and imminent threat to a person's or the public's health or safety.
                         </li>
                         <li>
-                        As Required By Law. We may use and disclose your PHI when required to do so by any other law not already referred to in the preceding categories.
+                        J. As Required By Law. We may use and disclose your PHI when required to do so by any other law not already referred to in the preceding categories.
                         </li>
                       </ul>
                     </div>                    
+                  </div>                  
+                </li>
+                <li className='w-full border-top border-t-2 border-[#3c4e3d]'>
+                  <div className='sm:flex justify-between space-y-7 sm:space-y-0 pt-10 sm:pt-20'>
+                    <h3 className='w-full md:max-w-[300px]'>Uses and Disclosures Requiring Your Written Authorization</h3>
+                    <ol className='space-y-7'>
+                      <li>
+                      A.	Use or Disclosure with Your Authorization. We must obtain your written authorization for most uses and disclosures of psychotherapy notes, uses and disclosures of PHI for marketing purposes, and disclosures that constitute the sale of PHI. Additionally, other uses and disclosures of PHI not described in this Notice will be made only when you give us your written permission on an authorization form (“Authorization”). For instance, you will need to complete and sign an authorization form before we can send your PHI to your life insurance company or to the attorney representing the other party in a lawsuit in which you are involved.
+                      </li>
+                      <li>
+                      B.	Uses and Disclosures of Your Highly Confidential Information. Federal and state law requires special privacy protections for certain highly confidential information about you (“Highly Confidential Information”). This Highly Confidential Information may include the subset of your PHI that: (1) is maintained in psychotherapy notes; (2) is about mental health and developmental disabilities services; (3) is about alcohol and drug abuse prevention, treatment and referral; (4) is about HIV/AIDS testing, diagnosis or treatment; (5) is about sexually-transmitted disease(s); (6) is about genetic testing; (7) is about child abuse and neglect; (7) is about domestic abuse of an adult with a disability; or (8) is about sexual assault. In order for us to disclose your Highly Confidential Information for a purpose other than those permitted by law, we must have your Authorization.
+                      </li>
+                      <li>
+                        C.	Revocation of Your Authorization. You may withdraw (revoke) your Authorization, or any written authorization regarding your Highly Confidential Information (except to the extent that we have taken action in reliance upon it) by contacting us at info@atria.org.
+                      </li>
+                    </ol>
+                  </div>                  
+                </li>
+                <li className='w-full border-top border-t-2 border-[#3c4e3d]'>
+                  <div className='sm:flex justify-between space-y-7 sm:space-y-0 pt-10 sm:pt-20'>
+                    <h3 className='w-full md:max-w-[300px]'>Your Rights Regarding Your Protected Health Information</h3>
+                    <ol className='space-y-7'>
+                      <li>
+                      A.	For Further Information; Complaints. If you would like more information about your privacy rights, if you are concerned that we have violated your privacy rights, or if you disagree with a decision that we made about access to your PHI, you may contact us. Also, you may make a complaint by calling us at 212-600-2000. You may also file written complaints with the Director, Office for Civil Rights of the U.S. Department of Health and Human Services. Upon request, we will provide you with the correct address for the Director. We will not retaliate against you if you file a complaint with us or the Director.
+                      </li>
+                      <li>
+                      B.	Right to Request Additional Restrictions. You have the right to request a restriction on the uses and disclosures of your PHI (1) for treatment, payment and health care operations purposes, and (2) to individuals (such as a family member, other relative, close personal friend or any other person identified by you) involved in your care or with payment related to your care. For example, you have the right to request that we not disclose your PHI to a health plan for payment or health care operations purposes, if that PHI pertains solely to a health care item or service for which we have been involved and which has been paid out of pocket in full. Unless otherwise required by law, we are required to comply with your request for this type of restriction. For all other requests for restrictions on use and disclosures of your PHI, we are not required to agree to your request, but will attempt to accommodate reasonable requests when appropriate. If you wish to request additional restrictions, you may submit a request by e-mailing us at info@atria.org. We may respond by sending you an e-mail or written response.
+                      </li>
+                      <li>
+                        C.	Right to Receive Confidential Communications. You may request, and we will accommodate, any reasonable written request for you to receive your PHI by alternative means of communication or at alternative locations.
+                      </li>
+                      <li>
+                        D.	Right to Inspect and Copy Your Health Information. You may request access to your medical record file and billing records maintained by us in order to inspect and request copies of the records. Under limited circumstances, we may deny you access to a portion of your records. If you would like to access your records, please obtain a Record Request Form available in the mobile application(s) or by e-mailing us at info@atria.org, and submit the completed form to the mobile application(s) or to us at info@atria.org. We will provide your records to you in electronic or hard copy form as requested by you. If you request copies, we will charge you a cost-based fee, consistent with applicable state law, that includes (1) labor for copying the PHI; (2) supplies for creating the paper copy or electronic media if you request an electronic copy on portable media; (3) our postage costs, if you request that we mail the copies to you; and (4) if you agree in advance, the cost of preparing an explanation or summary of the PHI.
+                      </li>
+                      <li>
+                        E.	Right to Amend Your Records. You have the right to request that we amend PHI maintained in your medical record file or billing records. If you desire to amend your records, please obtain an Amendment Request Form available in our mobile application(s) and submit the completed form via the mobile application(s). We will comply with your request unless we believe that the information that would be amended is accurate and complete or other special circumstances apply.
+                      </li>
+                      <li>
+                        F.	Right to Receive An Accounting of Disclosures. Upon request, you may obtain an accounting of certain disclosures of your PHI made by us during any period of time prior to the date of your request provided such period does not exceed six years.
+                      </li>
+                      <li>
+                        G.	Right to Receive A Copy of this Notice. Upon request, you may obtain a copy of this Notice, either by email or in paper format. Please submit your request us.
+                      </li>
+                    </ol>
                   </div>                  
                 </li>
                 <li className='w-full border-top border-t-2 border-[#3c4e3d]'>
