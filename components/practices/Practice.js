38ed9c23e212590031ab473b3e41beb566a02186
Jecy Jehu <jecyjehu@outlook.com>
2023-06-25 12:26:02 +0100

    deploy

--- components/practices/Practice.js
+++ components/practices/Practice.js
@@ -4,7 +4,7 @@ const Practice = () => {
   return (
     <section className='w-full h-auto my-20'>
         <div className='max-w-[420px] sm:max-w-[570px] md:max-w-[770px] lg:max-w-[1200px] m-auto'>
-            <ul className='list-[numbers] text-[#3c4e3d] text-lg font-semibold space-y-16 sm:space-y-36'>
+            <ul className='list-[numbers] text-[#3c4e3d] text-lg font-semibold leading-tight space-y-16 sm:space-y-36'>
                 <li className='w-full border-top border-t-2 border-[#3c4e3d]'>
                   <div className='sm:flex justify-between space-y-7 sm:space-y-0 pt-10 sm:pt-20'>
                     <h3 className='w-full md:max-w-[300px]'>Who We Are</h3>
@@ -14,15 +14,63 @@ const Practice = () => {
                 <li className='w-full border-top border-t-2 border-[#3c4e3d]'>
                   <div className='sm:flex justify-between space-y-7 sm:space-y-0 pt-10 sm:pt-20'>
                     <h3 className='w-full md:max-w-[300px]'>Our Privacy Obligations</h3>
-                    <p className='md:max-w-[1100px]'>We are required by law to maintain the privacy of your health information (“Protected Health Information” or “PHI”) and to provide you with this Notice of our legal duties and privacy practices with respect to your PHI.</p>
+                    <p className='md:max-w-[1100px]'>We are required by law to maintain the privacy of your health information (“Protected Health Information” or “PHI”) and to provide you with this Notice of our legal duties and privacy practices with respect to your PHI. <span className='text-[#3c4e3d]/60'>We are also obligated to notify you following a breach of unsecured PHI. When we use or disclose your PHI, we are required to abide by the terms of this Notice (or other notice in effect at the time of the use or disclosure).</span></p>
                   </div>                  
                 </li>
-                {/* <li className='w-full border-top border-t-2 border-[#3c4e3d]'>
+                <li className='w-full border-top border-t-2 border-[#3c4e3d]'>
                   <div className='sm:flex justify-between space-y-7 sm:space-y-0 pt-10 sm:pt-20'>
                     <h3 className='w-full md:max-w-[300px]'>Permissible Uses and Disclosures Without Your Written Authorisation</h3>
-                    <p className='md:max-w-[1100px]'>We are required by law to maintain the privacy of your health information (“Protected Health Information” or “PHI”) and to provide you with this Notice of our legal duties and privacy practices with respect to your PHI.</p>
+                    <div className='space-y-10'>
+                      <p className='md:max-w-[1100px]'>In certain situations, which we describe in Section IV below, we must obtain your written authorization in order to use and/or disclose your PHI. However, we do not need any type of authorization from you for the following uses and disclosures:</p>
+                      <ul className='list-[upper-roman] text-[#3c4e3d]/60 space-y-10'>
+                        <li>Uses and Disclosures For Treatment, Payment and Health Care Operations. We may use and disclose PHI, but not your “Highly Confidential Information” (defined in Section IV.B below), in order to treat you, obtain payment for services provided to you and conduct our “Health Care Operations” as detailed below:</li>
+                        <ol className='space-y-10'>
+                          <li className='list-disc list-inside'>
+                          Treatment. We may use and disclose your PHI to provide treatment, for example, to diagnose and treat your injury or illness. We may also disclose PHI to other health care providers involved in your treatment.
+                          </li>
+                          <li className='list-disc list-inside'>
+                          Payment. In most cases, we may use and disclose your PHI to partners that require we provide your insurance information for billing purposes. For example, we may disclose your PHI to external lab or other partners as they may be required under NY State or Federal law to bill your insurance company directly.
+                          </li>
+                          <li className='list-disc list-inside'>
+                          Health Care Operations. We may use and disclose your PHI for our health care operations, which include internal administration and planning and various activities that improve the quality and cost effectiveness of the care that we deliver to you. For example, we may use PHI to evaluate the quality and competence of our physicians and other health care practitioners. We may disclose PHI in order to resolve any complaints you may have. We may also use your PHI to create de-identified health information or limited data sets.
+                          </li>
+                          <li className='list-disc list-inside'>
+                          We may also disclose PHI to your other health care providers when such PHI is required for them to treat you, receive payment for services they render to you, or conduct certain health care operations, such as quality assessment and improvement activities, reviewing the quality and competence of health care professionals, or for health care fraud and abuse detection or compliance. For a list of other ways that we may use your PHI, you should check the privacy policies or terms of service for our website(s) and application(s).
+                          </li>
+                        </ol>
+                        <li className='space-y-10'>
+                          <p>Disclosure to Relatives, Close Friends and Other Caregivers. We may use or disclose your PHI to a family member, other relative, a close personal friend or any other person identified by you when you are present for, or otherwise available prior to, the disclosure, if we (1) obtain your agreement; (2) provide you with the opportunity to object to the disclosure and you do not object; or (3) reasonably infer that you do not object to the disclosure.</p>
+
+                          <p>If you are not present, or the opportunity to agree or object to a use or disclosure cannot practicably be provided because of your incapacity or an emergency circumstance, we may exercise our professional judgment to determine whether a disclosure is in your best interests. If we disclose information to a family member, other relative or a close personal friend, we would disclose only information that we believe is directly relevant to the person's involvement with your health care or payment related to your health care. We may also disclose your PHI in order to notify (or assist in notifying) such persons of your location, general condition, or death.</p>
+                        </li>
+                        <li>
+                        Public Health Activities. We may disclose your PHI for the following public health activities: (1) to report health information to public health authorities for the purpose of preventing or controlling disease, injury or disability; (2) to report child abuse and neglect to public health authorities or other government authorities authorized by law to receive such reports; (3) to report information about products and services under the jurisdiction of the U.S. Food and Drug Administration; (4) to alert a person who may have been exposed to a communicable disease or may otherwise be at risk of contracting or spreading a disease or condition; and (5) to report information to your employer as required under laws addressing work-related illnesses and injuries or workplace medical surveillance.
+                        </li>
+                        <li>
+                        Victims of Abuse, Neglect or Domestic Violence. If we reasonably believe you are a victim of abuse, neglect or domestic violence, we may disclose your PHI to a governmental authority, including a social service or protective services agency, authorized by law to receive reports of such abuse, neglect or domestic violence.
+                        </li>
+                        <li>
+                        Health Oversight Activities. We may disclose your PHI to a health oversight agency that oversees the health care system and is charged with responsibility for ensuring compliance with the rules of government health programs, such as Medicare or Medicaid.
+                        </li>
+                        <li>
+                        Judicial and Administrative Proceedings. We may disclose your PHI in the course of a judicial or administrative proceeding in response to a legal order or other lawful process.
+                        </li>
+                        <li>
+                        Law Enforcement Officers. We may disclose your PHI to the police or other law enforcement officials as required or permitted by law or in compliance with a court order or a grand jury or administrative subpoena.
+                        </li>
+                        <li>
+                        Organ and Tissue Procurement. We may disclose your PHI to organizations that facilitate organ, eye or tissue procedure, banking or transplantation.
+                        </li>
+                        <li>
+                        Health or Safety. We may use or disclose your PHI to prevent or lessen a serious and imminent threat to a person's or the public's health or safety.
+                        </li>
+                        <li>
+                        As Required By Law. We may use and disclose your PHI when required to do so by any other law not already referred to in the preceding categories.
+                        </li>
+                      </ul>
+                    </div>                    
                   </div>                  
-                </li> */}
+                </li>
             </ul>
         </div>
     </section>
