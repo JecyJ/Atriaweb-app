c7b1f192c54636afad836b555d1f74504b5a44d9
Jecy Jehu <jecyjehu@outlook.com>
2023-06-25 10:53:54 +0100

    deploy

--- components/practices/Hero.js
+++ components/practices/Hero.js
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <section className='w-full h-auto pt-48 lg:pt-64'>
         <div className='max-w-[520px] sm:max-w-[620px] md:max-w-[1200px] m-auto'>
-            <h1 className='text-5xl sm:text-6xl lg:text-8xl text-[#3c4e3d] md:text-center font-medium'>Notice of <br className='md:hidden' /> Privacy <br /> Practices</h1>
+            <h1 className='text-5xl sm:text-6xl lg:text-8xl text-[#3c4e3d] md:text-center font-semibold'>Notice of <br className='md:hidden' /> Privacy <br /> Practices</h1>
         </div>
     </section>
   )
--- components/practices/Notice.js
+++ components/practices/Notice.js
@@ -7,7 +7,7 @@ const Notice = () => {
         <div className="max-w-[450px] md:max-w-[1200px] m-auto">
             <ul className='border-t border-t-[#3c4e3d] border-t-3  my-20'>
                 <li className='text-[#3c4e3d] py-8'><BsCircleFill size={28}  /></li>
-                <li className='relative md:z-[2] text-[#3c4e3d] text-5xl lg:text-6xl font-semibold'>This notice describes how medical information about you may be used and disclosed and how you can get access to this information. </li>
+                <li className='relative md:z-[2] text-[#3c4e3d] text-5xl lg:text-6xl font-medium'>This notice describes how medical information about you may be used and disclosed and how you can get access to this information. </li>
             </ul>
         </div>
     </section>
