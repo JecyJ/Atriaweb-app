6903998b56790e96c093fa6988aeca8ebd87a662
Jecy Jehu <jecyjehu@outlook.com>
2023-06-30 10:37:14 +0100

    deploy

--- components/academy/hero.js
+++ components/academy/hero.js
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <section className='grid content-center pt-72 sm:pt-20 lg:pt-72 w-full h-screen'>
       <div className='z-[-2]'>
-        <Image src='/assets/images/stetoscope.png' alt='/' fill cover />
+        <Image src='/assets/images/stetoscope.png' alt='/' fill cover="true" />
       </div>
       <div className='max-w-[520px] sm:max-w-[620px] md:max-w-[1250px] w-full m-auto space-y-14 sm:space-y-8 text-white'>
         <h1 className='text-lg lg:text-3xl'>Guiding a transformation in medicine</h1>
